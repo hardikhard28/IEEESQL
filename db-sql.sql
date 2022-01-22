9ce6a594a765ad63fdec1191f82e79b8ecdc7324
hardikhard28 <63140787+hardikhard28@users.noreply.github.com>
2022-01-22 12:29:55 +0530

    Update db-sql.sql

--- db-sql.sql
+++ db-sql.sql
@@ -1,42 +1,18 @@
 
 --
 -- Database: `db_contact`
---
-
-CREATE DATABASE IF NOT EXISTS `db_contact` DEFAULT CHARACTER SET latin1 COLLATE latin1_swedish_ci;
-USE `db_contact`;
+--create a db called db_contact;
 
 -- --------------------------------------------------------
 
 --
 -- Table structure for table `tbl_contact`
 --
+-- create a table tbl_contact with the following columns
+-- id,fldName,fldEmail,fldPhone,fldMessage;
+-- DROP TABLE IF EXISTS `tbl_contact`;
 
-DROP TABLE IF EXISTS `tbl_contact`;
-CREATE TABLE `tbl_contact` (
-  `id` int(11) NOT NULL,
-  `fldName` varchar(50) NOT NULL,
-  `fldEmail` varchar(150) NOT NULL,
-  `fldPhone` varchar(15) NOT NULL,
-  `fldMessage` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `tbl_contact`
---
-ALTER TABLE `tbl_contact`
- ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `tbl_contact`
 --
 ALTER TABLE `tbl_contact`
 MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
